1c4f65868227d0eab7fa7a2e6a021c2d9faaa84c
Dovydas Ceilutka <10526751+mokahaiku@users.noreply.github.com>
2020-03-05 13:31:03 +0200

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,19 @@
+sudo apt-get update --yes
+sudo apt-get install --yes \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update --yes
+sudo apt-get install --yes docker-ce docker-ce-cli containerd.io
+sudo groupadd docker
+sudo usermod -aG docker $USER
+sudo systemctl enable docker
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
